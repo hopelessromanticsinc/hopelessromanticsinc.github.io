b086dfc8bd8eb0ee4fa9ee367fc828bedd232a8a
Dimitri.Fankhauser <dimitri.fankhauser@gmail.com>
2024-02-29 22:49:22 +0100

    feature: started reading content strings from json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ExampleComponent } from './components/example/example.component';
 import { AboutComponent } from './components/about/about.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    HomeComponent,
     NavComponent,
     HeaderComponent,
     FooterComponent,
     ExampleComponent,
     AboutComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
   providers: [],
-  bootstrap: [HomeComponent, NavComponent, HeaderComponent, FooterComponent],
+  bootstrap: [ NavComponent, HeaderComponent, FooterComponent],
 })
 export class AppModule {}
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {}
+export class AboutComponent implements OnInit {
+  public textContent: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.http.get('/assets/content.json').subscribe(res => {
+      this.textContent = res
+    })
+  }
+}

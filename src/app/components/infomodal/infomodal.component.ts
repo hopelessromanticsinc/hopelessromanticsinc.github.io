b435c990706ecf8804b85d0142942fae533ea7e5
Dimitri.Fankhauser <dimitri.fankhauser@gmail.com>
2024-03-04 16:21:52 +0100

    build for deployment on github pages

---
+++ src/app/components/infomodal/infomodal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infomodal',
+  templateUrl: './infomodal.component.html',
+  styleUrls: ['./infomodal.component.scss'],
+})
+export class InfomodalComponent {}
